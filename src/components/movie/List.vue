24e05a6e6762de1fad14f86fdca23357d8111789
Khushbu <pkhush1993@gmail.com>
2021-04-28 08:28:30 +0530

    WIP - spinner style modifications

--- src/components/movie/List.vue
+++ src/components/movie/List.vue
@@ -5,18 +5,21 @@
         <b>Results for "{{ searchText }}"</b>
       </p>
       <div
-          v-if="isFetching"
-          class="spinner-border text-secondary"
-          style="
-            width: 3rem;
-            height: 3rem;
-            position: absolute;
-            z-index: 100;
-            top: 40%;
-            left: 45%;
-          "
-          role="status"
-        ></div>
+        v-if="isFetching"
+        class="spinner-border text-secondary"
+        style="
+          width: 3rem;
+          height: 3rem;
+          position: absolute;
+          z-index: 100;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          margin: auto;
+        "
+        role="status"
+      ></div>
       <ul>
         <li
           class="p-2"
@@ -24,14 +27,15 @@
           :key="searchResult.imdbID"
         >
           {{ searchResult.Title }} ( {{ searchResult.Year }} )
-          <button :disabled="isNominated(searchResult.imdbID)"
+          <button
+            :disabled="isNominated(searchResult.imdbID)"
             class="btn btn-sm btn-secondary"
             @click="nominateMovie(searchResult)"
           >
             Nominate
           </button>
         </li>
-        <li v-if="(searchResults.length == 0)">No results found</li>
+        <li v-if="searchResults.length == 0">No results found</li>
       </ul>
     </div>
   </div>
@@ -53,7 +57,7 @@ export default {
   },
   methods: {
     isNominated(imdbID) {
-      return this.nominatedMovies.find((movie) => movie.imdbID === imdbID)
+      return this.nominatedMovies.find((movie) => movie.imdbID === imdbID);
     },
     nominateMovie(imdbID) {
       this.$emit("nominateMovieParent", imdbID);
